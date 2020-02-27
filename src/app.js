893230b513899bad1668fdcd757017e49b6415d1
Guillaume <guillaume.brulotte@gmail.com>
2020-02-27 17:46:20 -0500

    Cleaned writeFileSync

--- src/app.js
+++ src/app.js
@@ -49,7 +49,7 @@ app.post("/transfer", (req, res) => {
   accounts[accountTo].balance += parseInt(transferAmount);
 
   const accountsJSON = JSON.stringify(accounts);
-  fs.writeFileSync(path.join(__dirname, "json/accounts.json"), accountsJSON, { encoding: "UTF8" });
+  fs.writeFileSync(path.join(__dirname, "json", "accounts.json"), accountsJSON, "utf8");
 
   res.render("transfer", { message: "Transfer Completed" });
 });
@@ -62,8 +62,8 @@ app.post("/payment", (req, res) => {
   accounts.credit.balance -= parseInt(req.body.amount);
   accounts.credit.available += parseInt(req.body.amount);
 
-  const accountsJSON = JSON.stringify(accounts);
-  fs.writeFileSync(path.join(__dirname, "json/accounts.json"), accountsJSON, { encoding: "UTF8" });
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(path.join(__dirname, "json", "accounts.json"), accountsJSON, "utf8");
 
   res.render("payment", { message: "Payment Successful", account: accounts.credit });
 });
