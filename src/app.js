4b765f2ceaf3304c50695e1a30e5cf1763234c1e
Guillaume <guillaume.brulotte@gmail.com>
2020-02-27 16:17:17 -0500

    Updated directory names and use right port number

--- src/app.js
+++ src/app.js
@@ -4,15 +4,15 @@ const express = require("express");
 
 const app = express();
 
-app.set("views", path.join(__dirname, "./views/"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, "./public/")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Index" });
 });
 
-app.listen(30001, () => {
+app.listen(3000, () => {
   console.log("PS Project Running on port 3000!");
 });
