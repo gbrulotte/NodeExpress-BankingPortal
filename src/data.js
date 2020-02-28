6627fb32f121879a435849c61198812a9c9df0f0
Guillaume <guillaume.brulotte@gmail.com>
2020-02-28 10:43:51 -0500

    Module 4 extract json serialization to data.js

---
+++ src/data.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const path = require("path");
+
+const accountData = fs.readFileSync(path.join(__dirname, "/json/accounts.json"), { encoding: "UTF8" });
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname, "/json/users.json"), { encoding: "UTF8" });
+const users = JSON.parse(userData);
+
+function writeJSON() {
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(path.join(__dirname, "json", "accounts.json"), accountsJSON, "utf8");
+
+  const usersJSON = JSON.stringify(users, null, 4);
+  fs.writeFileSync(path.join(__dirname, "json", "users.json"), usersJSON, "utf8");
+}
+
+module.exports = { accounts, users, writeJSON };
